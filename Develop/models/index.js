f8427a655cbae70b026d8667c56bff0c5d786efd
HobbZ <zachobba@gmail.com>
2021-11-18 08:49:25 +1000

    set up Belongs to and has options

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+})
 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+})
 
 module.exports = {
   Product,
